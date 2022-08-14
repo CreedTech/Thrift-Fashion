bc5b47e31a92b746db97734ce6bc1cfaa218b621
CreedTech <ayodeleayoolaoke4@gmail.com>
2022-08-14 12:48:28 +0100

    finalizing codes

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Item, OrderItem, Order, Payment, Coupon, Refund, Address, UserProfile
+from .models import Category, Contact, Feedback, Item, OrderItem, Order, Payment, Coupon, Refund, Address, UserProfile
 
 
 def make_refund_accepted(modeladmin, request, queryset):
@@ -57,6 +57,8 @@ class AddressAdmin(admin.ModelAdmin):
 
 admin.site.register(Category)
 admin.site.register(Item)
+admin.site.register(Feedback)
+admin.site.register(Contact)
 admin.site.register(OrderItem)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Payment)
--- core/forms.py
+++ core/forms.py
@@ -109,6 +109,32 @@ class FeedbackForm(forms.ModelForm):
         model = Feedback
         fields = '__all__'
 
+    def get_info(self):
+
+        # Cleaned data
+        cl_data = super().clean()
+
+        name = cl_data.get('name').strip()
+        from_email = cl_data.get('email')
+        subject = cl_data.get('subject')
+
+        msg = f'{name} with email {from_email} said:'
+        msg += f'\n"{subject}"\n\n'
+        msg += cl_data.get('message')
+
+        return subject, msg
+
+    def send(self):
+
+        subject, msg = self.get_info()
+
+        send_mail(
+            subject=subject,
+            message=msg,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[settings.RECIPIENT_ADDRESS]
+        )
+
 
 class CheckoutForm(forms.Form):
     shipping_address = forms.CharField(required=False)
